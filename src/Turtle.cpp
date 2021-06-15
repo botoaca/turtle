b2b3a38f760bec33f1e89047280be98eb9b60345
nocturnodev <botoaca.mihnea@gmail.com>
2021-06-15 23:58:19 +0300

    removed excess code from old system

--- src/Turtle.cpp
+++ src/Turtle.cpp
@@ -20,26 +20,4 @@ void Turtle::run() {
 
     Parser parser(tokens);
     parser.parse();
-
-
-    // int lineCount = 0;
-    // int argSize;
-
-    // while (std::getline(input, line)) { // Executes instructions for each line
-    //     lineCount++;
-    //     if (line[0] != '#')
-    //         argSize = line[0] - '0';
-
-    //     // CALC function
-    //     if (line[2] == 'C' && line[3] == 'A' && line[4] == 'L' && line[5] == 'C')
-    //         i.calculate(line);
-
-    //     // INPUT function
-    //     if (line[2] == 'I' && line[3] == 'N')
-    //         i.input(line, argSize);
-
-    //     // OUTPUT function
-    //     if (line[2] == 'O' && line[3] == 'U' && line[4] == 'T' && line[6] == '"')
-    //         i.output(line, argSize, lineCount);
-    // }
 }
